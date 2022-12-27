c69956b671bb732eb67b6ecc9ecbdc44f92ccc9e
Natalia <dginni2009@gmail.com>
2022-12-27 14:24:14 +0300

    initial commit

---
+++ task50/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect input");
+        }
+
+    }
+
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int rows = GetNumber("Enter the number of rows");
+int columns = GetNumber("Enter the number of columns");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+
+
+int indexA = GetNumber("Enter the first index of the position");
+int indexB = GetNumber("Enter the second index of the position");
+
+if(indexA > matrix.GetLength(0) || indexB > matrix.GetLength(1))
+        Console.WriteLine("There is no such element");
+else
+        Console.WriteLine(matrix[indexA, indexB]);
---
+++ task52/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect input");
+        }
+
+    }
+
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void GetColumnAverage(int[,] matrix)
+{
+    
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+                
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average = matrix[i, j] + average;
+            
+        }
+        average = average / matrix.GetLength(1);
+        Console.Write(Math.Round(average, 1) + "; ");
+    }
+       
+}
+
+int rows = GetNumber("Enter the number of rows");
+int columns = GetNumber("Enter the number of columns");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+GetColumnAverage(matrix);
